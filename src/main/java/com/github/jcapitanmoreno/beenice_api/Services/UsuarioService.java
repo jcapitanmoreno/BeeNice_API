8b5ede2d92444fdb40971318e12bbe1a9737678d
jcapitanmoreno <151913646+jcapitanmoreno@users.noreply.github.com>
2025-04-09 13:43:15 +0200

    usuario service en camino

--- src/main/java/com/github/jcapitanmoreno/beenice_api/Services/UsuarioService.java
+++ src/main/java/com/github/jcapitanmoreno/beenice_api/Services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.github.jcapitanmoreno.beenice_api.Services;
 
+import com.github.jcapitanmoreno.beenice_api.exceptions.RecordNotFoundException;
 import com.github.jcapitanmoreno.beenice_api.models.Usuario;
 import com.github.jcapitanmoreno.beenice_api.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -21,5 +23,19 @@ public class UsuarioService {
         } else {
             return new ArrayList<>();
         }
+    }
+
+    public Usuario getUsuarioById(Long id)  throws RecordNotFoundException {
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+        if (usuario.isPresent()) {
+            return usuario.get();
+        } else {
+            throw new RecordNotFoundException("No existe usuario para el id ", id);
+        }
+    }
+
+    public Usuario createUsuario(Usuario usuario) {
+        usuario = usuarioRepository.save(usuario);
+        return usuario;
     }
 }
